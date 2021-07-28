cb0715152079a6b570bd34d1cec2bdadd2f912ae
Dima Tisnek <dimaqq@gmail.com>
2021-07-28 22:16:06 +0900

    cleanup🧼

--- src/index.js
+++ src/index.js
@@ -1,7 +1,65 @@
 // @flow
 import React from 'react';
+import type {Node} from "react";
+import styled, {createGlobalStyle} from "styled-components";
 import ReactDOM from 'react-dom';
-import App from './App';
+
+const App = (): Node =>
+    <Box>
+      <GlobalStyle/>
+      <header className="App-header">
+        <p>
+          Edit blah blah blah...
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </Box>;
+
+const Box = styled.div`
+  border: 5px solid var(--red);
+  padding: 10px;
+`;
+
+const GlobalStyle = createGlobalStyle`
+body[data-theme='light'] {
+  --colors-primary: deeppink;
+  --colors-background: white;
+}
+body[data-theme='dark'] {
+  --colors-primary: lightpink;
+  --colors-background: black;
+}
+
+:root {
+  --back: #fff;
+  --backish: #aaa;
+  --text: #222;
+
+  --red: #d00;
+}
+
+@media (prefers-color-scheme: dark) {
+  :root {
+    --back: #000;;
+    --backish: #222;
+    --text: #aaa;
+
+    --red: #f66;
+  }
+}
+
+body {
+  color: var(--text);
+  background-color: var(--back);
+}
+`;
 
 ReactDOM.render(
   <React.StrictMode>
